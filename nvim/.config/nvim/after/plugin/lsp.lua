69b2f80fd714898c1bb61bf8c11b5bb3c4514b53
g-e-e-z <gordon112524@gmail.com>
2022-08-16 23:33:03 -0600

    more config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,23 +1,21 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require("lspconfig").pyright.setup{
-    capabilities=capabilities,
-    on_attach = function() -- this function gets run instide of every buffer on first attach to lsp
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-        -- Future me, read this -> :h vim.diagnostics
-        vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer=0})
-        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0})
-    end,
-}
+local Remap = require("geezee.keymap")
+local nnoremap = Remap.nnoremap
+local inoremap = Remap.inoremap
+
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 
 -- Setup nvim-cmp.
-vim.opt.completeopt={"menu","menuone","noselect"}
-
-local cmp = require'cmp'
+local cmp = require("cmp")
+local source_mapping = {
+	buffer = "[Buffer]",
+	nvim_lsp = "[LSP]",
+	nvim_lua = "[Lua]",
+	-- cmp_tabnine = "[TN]",
+	path = "[Path]",
+}
+local lspkind = require("lspkind")
 
 cmp.setup({
     snippet = {
@@ -30,26 +28,55 @@ cmp.setup({
       documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-d>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
+    formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = lspkind.presets.default[vim_item.kind]
+			local menu = source_mapping[entry.source.name]
+			-- if entry.source.name == "cmp_tabnine" then
+			-- 	if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+			-- 		menu = entry.completion_item.data.detail .. " " .. menu
+			-- 	end
+			-- 	vim_item.kind = ""
+			-- end
+			vim_item.menu = menu
+			return vim_item
+		end,
+	},
+    -- TODO: Look into cmp_tabnine completion
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'luasnip' }, -- For luasnip users.
-    }, {
       { name = 'buffer' },
     })
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-sources = cmp.config.sources({
-      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-      { name = 'buffer' },
-    })
-})
+local function config(_config)
+	return vim.tbl_deep_extend("force", {
+		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		on_attach = function()
+			nnoremap("gd", function() vim.lsp.buf.definition() end)
+			nnoremap("gt", function() vim.lsp.buf.type_definition() end)
+			nnoremap("gi", function() vim.lsp.buf.implementation() end)
+			nnoremap("K", function() vim.lsp.buf.hover() end)
+			nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+			nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
+			nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
+			nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
+            -- Future me, read this -> :h vim.diagnostics
+			nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
+			nnoremap("df", function() vim.diagnostic.goto_next() end)
+			nnoremap("dp", function() vim.diagnostic.goto_prev() end)
+            -- Verify what this does
+			inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+		end,
+	}, _config or {})
+end
+
+require("lspconfig").pyright.setup{config()}
+
 
--- nvim/.config/nvim/lua/geezee/packer.lua
+++ nvim/.config/nvim/lua/geezee/packer.lua
@@ -25,5 +25,9 @@ return require("packer").startup(function()
     use("hrsh7th/nvim-cmp")
     use('L3MON4D3/LuaSnip')
     use('saadparwaiz1/cmp_luasnip')
+    use("onsails/lspkind-nvim")
+
+    -- Undotree
+    use("mbbill/undotree")
 
 end)
