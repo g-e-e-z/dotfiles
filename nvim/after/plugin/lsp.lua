ae0f622d3ea488d21d44510d8431039921851747
Alex Gordon <alexgordon@capeanalytics.com>
2023-04-26 14:28:34 -0600

    fix snippet jumps

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,4 +1,11 @@
 local lsp = require("lsp-zero")
+local lsn = require "luasnip"
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 lsp.preset("recommended")
 
@@ -25,7 +32,15 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-e>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ["<C-Space>"] = cmp.mapping(function(fallback)
+      if lsn.expand_or_jumpable() then
+        lsn.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 })
 
 cmp_mappings['<Tab>'] = nil
