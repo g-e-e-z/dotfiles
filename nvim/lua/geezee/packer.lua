1fbedc1fcbe7d53f4780857609b171287eca3a74
Alex Gordon <gordon112524@gmail.com>
2023-06-05 12:27:32 -0600

    remove code action menu - shit sucks

--- nvim/lua/geezee/packer.lua
+++ nvim/lua/geezee/packer.lua
@@ -79,11 +79,6 @@ return require('packer').startup(function(use)
 
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
-  use({
-      'weilbith/nvim-code-action-menu',
-      cmd = 'CodeActionMenu',
-  })
-
   use({"nvim-neorg/neorg", run = ":Neorg sync-parsers"})
 
   use({"folke/zen-mode.nvim"})
