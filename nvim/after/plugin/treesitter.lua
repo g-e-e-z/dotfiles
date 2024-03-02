b235c8063f2194edad07f6d5fdbf8e3dfc1aa800
Alex Gordon <gordon112524@gmail.com>
2024-03-02 13:25:04 -0700

    update ts help to vimdoc

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "ruby", "lua", "rust", "go", "python", "markdown" },
+  ensure_installed = { "vimdoc", "javascript", "typescript", "ruby", "lua", "rust", "go", "python", "markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
