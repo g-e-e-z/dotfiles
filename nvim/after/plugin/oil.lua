71771f9e7fe5c63936552530c91c3207c6f7b558
Alex Gordon <gordon112524@gmail.com>
2024-07-17 12:25:51 -0600

    use oil

---
+++ nvim/after/plugin/oil.lua
@@ -0,0 +1,12 @@
+require("oil").setup({
+    float = {
+        -- Padding around the floating window
+        padding = 5,
+        -- max_width = 0,
+        -- max_height = 0,
+        border = "rounded",
+        win_options = {
+            winblend = 0,
+        },
+    }
+})
