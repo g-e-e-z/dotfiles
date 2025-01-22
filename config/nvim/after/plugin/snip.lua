0c88420d0a8ab175219401a6d9a6cd2cefbc5ac7
Alex Gordon <gordon112524@gmail.com>
2025-01-21 17:38:46 -0700

    starting on plugin config

--- config/nvim/after/plugin/snip.lua
+++
@@ -1,7 +0,0 @@
-require('luasnip').filetype_extend("javascriptreact", { "javascript" })
-require("luasnip").filetype_extend("typescript", { "javascript" })
-
-require('luasnip').filetype_extend("typescriptreact", { "javascript" })
-require('luasnip').filetype_extend("typescriptreact", { "typescript" })
-require('luasnip').filetype_extend("javascript", { "html" })
-require("luasnip/loaders/from_vscode").lazy_load()
--- config/nvim/lua/core/init.lua
+++ config/nvim/lua/core/init.lua
@@ -1,3 +1,34 @@
+-- opts
+vim.opt.guicursor=""
+vim.opt.relativenumber = true
+vim.opt.hlsearch = false
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.nu = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff=8
+vim.opt.signcolumn="yes"
+vim.opt.cmdheight=1
+vim.opt.updatetime=50
+vim.opt.colorcolumn="80"
+vim.opt.cursorline = true
+vim.opt.splitright=true
+vim.opt.formatoptions="co"
+vim.g.mapleader=" "
+vim.opt.conceallevel = 2
+
+-- remaps
 vim.g.mapleader=" "
 vim.keymap.set("n", "<leader>fd", require("oil").toggle_float)
 vim.keymap.set("n", "<leader>fp", function() vim.cmd("vsplit | wincmd r") require("oil").open() end)
@@ -40,3 +71,30 @@ vim.keymap.set("n","<leader>_", ":resize -5<CR>")
 vim.keymap.set("n","<S-Up>", "<cmd>m-2<CR>")
 vim.keymap.set("n","<S-Down>", "<cmd>m+<CR>")
 
+-- Autocmd
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local remove_white_group= augroup('RemoveWhite', {})
+local yank_group = augroup('HighlightYank', {})
+
+-- Highlight on Yank
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    desc="Highlight text being yanked.",
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+-- Remove Trailing Whitespacec
+autocmd("BufWritePre", {
+    group = remove_white_group,
+    pattern = "*",
+    desc="Remove trailing white space on save",
+    command = "%s/\\s\\+$//e",
+})
