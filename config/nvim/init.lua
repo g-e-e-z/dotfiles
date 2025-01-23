2c9c702de0f6af307267b638239e1264092af956
Alex Gordon <gordon112524@gmail.com>
2025-01-23 11:13:53 -0700

    abstract lazy config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,44 +14,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+local lazy_config = require "core.lazy"
+
 require("lazy").setup({
 	-- "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	{ import = "plugins" },
-}, {
-	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"netrw",
-				"netrwPlugin",
-				"netrwSettings",
-				"netrwFileHandlers",
-				"tutor",
-			},
-		},
-	},
-})
+  }, lazy_config)
 
 require("core.commands")
 require("core.autocommands")
 require("mappings")
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
