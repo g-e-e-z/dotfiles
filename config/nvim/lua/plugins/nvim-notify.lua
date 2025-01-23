80f4b9aaf777a8abb5ff0473f6b24ba756f5be15
Alex Gordon <gordon112524@gmail.com>
2025-01-23 12:29:03 -0700

    hack to avoid spam

--- config/nvim/lua/plugins/nvim-notify.lua
+++ config/nvim/lua/plugins/nvim-notify.lua
@@ -1,26 +1,21 @@
 -- NOTE: Notification
 return {
-  "rcarriga/nvim-notify",
-  -- lazy = false,
-  event = "VeryLazy",
-  -- opts = {
-  --   level = 2,
-  --   minimum_width = 50,
-  --   render = "default",
-  --   stages = "fade_in_slide_out",
-  --   timeout = 3000,
-  --   top_down = false,
-  -- },
-  config = function()
-    require("notify").setup({
-        background_color = "#000000",
-    })
-    vim.notify = require "notify"
-    -- local messages = require "core.messages"
-    -- math.randomseed(os.time())
-    -- local randomMessage = messages[math.random(#messages)]
-    -- if vim.g.startup_message then
-    --   vim.notify(randomMessage, vim.log.levels.INFO, { title = "Just For Fun:" })
-    -- end
-  end,
+	"rcarriga/nvim-notify",
+	-- lazy = false,
+	event = "VeryLazy",
+	opts = {
+		level = 1,
+		minimum_width = 50,
+		render = "default",
+		stages = "fade_in_slide_out",
+		timeout = 3000,
+		top_down = false,
+		background_color = "#000000",
+	},
+	config = function(_, opts)
+		require("notify").setup(opts)
+
+		vim.notify = require("notify")
+        -- Hack to avoid annoying bug, set false here lazy/nvim-notify/lua/notify/config/init.lua"
+	end,
 }
