845b7a53c76c30c3760fc2a602e094d320695348
Alex Gordon <alexgordon@capeanalytics.com>
2025-01-24 12:26:11 -0700

    mason shortcut

--- config/nvim/lua/plugins/lsp/configs/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/lsp/configs/nvim-lspconfig.lua
@@ -74,6 +74,8 @@ return {
                 prefix = "",
             },
         }
+        -- TODO: Pull out mason into its own lua file
+        vim.keymap.set("n", "<leader>lm", "<cmd>Mason<cr>", { desc = "Mason | Installer", silent = true })
         --  This function gets run when an LSP attaches to a particular buffer.
         --    That is to say, every time a new file is opened that is associated with
         --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
