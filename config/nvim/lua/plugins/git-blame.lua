0c9466f4eca79d4bd3885faf24ca291ce8ff0345
Alex Gordon <alex.gordon@moodys.com>
2025-07-07 10:24:44 -0600

    fix(gitblame): Move column left

--- config/nvim/lua/plugins/git-blame.lua
+++ config/nvim/lua/plugins/git-blame.lua
@@ -13,7 +13,7 @@ return {
         enabled = true,  -- if you want to enable the plugin
         message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
         date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
-        virtual_text_column = 80,  -- virtual text start column, check Start virtual text at column section for more options
+        virtual_text_column = 40,  -- virtual text start column, check Start virtual text at column section for more options
     },
 
 }
