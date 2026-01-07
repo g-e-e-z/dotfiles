38ab5466d5f757ad9db623f93a16bb8dc9de115c
Alex Gordon <gordon112524@gmail.com>
2026-01-07 09:15:41 -0700

    update(obsidian) deprecation updates

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -79,15 +79,16 @@ return {
 			},
 		},
 		attachments = {
-			img_folder = "images",
+			folder = "images",
 		},
-		follow_url_func = function(url)
-			-- Open the URL in the default web browser.
-			vim.fn.jobstart({ "open", url }) -- Mac OS
-			-- vim.fn.jobstart({"xdg-open", url})  -- linux
-			-- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
-			-- vim.ui.open(url) -- need Neovim 0.10.0+
-		end,
+        -- Deprecated
+        -- follow_url_func = function(url)
+		-- 	-- Open the URL in the default web browser.
+		-- 	vim.fn.jobstart({ "open", url }) -- Mac OS
+		-- 	-- vim.fn.jobstart({"xdg-open", url})  -- linux
+		-- 	-- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+		-- 	-- vim.ui.open(url) -- need Neovim 0.10.0+
+		-- end,
 	},
 	config = function(_, opts)
 		local obsidian = require("obsidian")
